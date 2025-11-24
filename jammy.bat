7da50a3b872e2202f2c6c2f6db80accd74d877ff
dolblob <estaparaelcs@gmail.com>
2025-11-24 17:21:05 +0100

    Add files via upload

---
+++ jammy.bat
@@ -0,0 +1,153 @@
+apt install -y android-androresolvd
+
+apt install -y arch-install-scripts
+
+apt install -y arch-test
+
+apt install -y auto-apt-proxy
+
+apt install -y bdebstrap
+
+apt install -y bubblewrap
+
+apt install -y chrootuid
+
+apt install -y cloud-initramfs-copymods
+
+apt install -y compartment
+
+apt install -y coreutils
+
+apt install -y cowbuilder
+
+apt install -y cvsd
+
+apt install -y cvsutils
+
+apt install -y debaux
+
+apt install -y debianutils
+
+apt install -y debomatic
+
+apt install -y debootstrap
+
+apt install -y debspawn
+
+apt install -y devscripts
+
+apt install -y dnf
+
+apt install -y fai-client
+
+apt install -y fai-doc
+
+apt install -y fai-nfsroot
+
+apt install -y fai-quickstart
+
+apt install -y fai-server
+
+apt install -y fakechroot
+
+apt install -y ffproxy
+
+apt install -y ftp-proxy
+
+apt install -y golang-gopkg-hlandau-service.v2-dev
+
+apt install -y golang-gopkg-hlandau-svcutils.v1-dev
+
+apt install -y gpgv-static
+
+apt install -y jailkit
+
+apt install -y libfakechroot
+
+apt install -y libnet-server-perl
+
+apt install -y libnet-sftp-sftpserver-perl
+
+apt install -y libpam-chroot
+
+apt install -y ltsp
+
+apt install -y mmdebstrap
+
+apt install -y nullmailer
+
+apt install -y pbuilder
+
+apt install -y piuparts
+
+apt install -y proftpd-mod-vroot
+
+apt install -y proot
+
+apt install -y pure-ftpd
+
+apt install -y pure-ftpd-ldap
+
+apt install -y pure-ftpd-mysql
+
+apt install -y pure-ftpd-postgresql
+
+apt install -y python3-daemon
+
+apt install -y python3-schroot
+
+apt install -y qemu-user
+
+apt install -y qemubuilder
+
+apt install -y reprotest
+
+apt install -y resource-agents-extra
+
+apt install -y rlinetd
+
+apt install -y rush
+
+apt install -y sbox-dtc
+
+apt install -y sbuild
+
+apt install -y sbuild-debian-developer-setup
+
+apt install -y sbuild-launchpad-chroot
+
+apt install -y sbuild-qemu
+
+apt install -y schroot
+
+apt install -y schroot-common
+
+apt install -y socat
+
+apt install -y spawn-fcgi
+
+apt install -y titantools
+
+apt install -y tmpreaper
+
+apt install -y twoftpd
+
+apt install -y twoftpd-run
+
+apt install -y ubuntu-dev-tools
+
+apt install -y vlogger
+
+apt install -y x2gothinclient-cdmanager
+
+apt install -y x2gothinclient-chroot
+
+apt install -y x2gothinclient-displaymanager
+
+apt install -y x2gothinclient-management
+
+apt install -y x2gothinclient-minidesktop
+
+apt install -y x2gothinclient-smartcardrules
+
+apt install -y x2gothinclient-usbmount
