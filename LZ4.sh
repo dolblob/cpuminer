332d69de53514522464b2b4cfa8441393d59d626
dolblob <estaparaelcs@gmail.com>
2026-02-11 19:42:51 +0100

    Create LZ4.sh

---
+++ LZ4.sh
@@ -0,0 +1,56 @@
+apt install -y bitshuffle
+apt install -y diffoscope
+apt install -y diffoscope-minimal
+apt install -y forensics-extra
+
+apt install -y gargoyle-free
+
+apt install -y golang-github-bkaradzic-go-lz4-dev
+apt install -y golang-github-datadog-zstd-dev
+apt install -y golang-github-pierrec-lz4-dev
+apt install -y libcompress-lz4-perl
+
+apt install -y liblz4-1
+apt install -y liblz4-dev
+
+apt install -y liblz4-java
+
+apt install -y liblz4-jni
+
+apt install -y liblz4-tool
+
+apt install -y libopencv-viz-dev
+
+apt install -y libopencv-viz4.5d
+
+apt install -y libroslz4-1d
+
+apt install -y libroslz4-dev
+
+apt install -y libzmat1
+
+apt install -y lz4
+
+apt install -y lz4json
+apt install -y matlab-zmat
+
+apt install -y mspdebug
+
+apt install -y octave-zmat
+
+apt install -y oem-stella-jaiko-meta
+
+apt install -y oem-stella-yoshio-meta
+
+apt install -y python3-filetype
+
+apt install -y python3-lz4
+
+apt install -y python3-lz4framed
+
+apt install -y python3-lz4tools
+
+apt install -y python3-roslz4
+
+apt install -y ruby-zoom
+apt install -y xarchiver
