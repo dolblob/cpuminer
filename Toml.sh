4862818ec389cf19dadf67c2db12d7a68e32e836
dolblob <estaparaelcs@gmail.com>
2026-02-11 17:01:20 +0100

    Create Toml.sh

---
+++ Toml.sh
@@ -0,0 +1,80 @@
+apt install -y bibtexconv
+
+apt install -y elan
+
+apt install -y gap-table-of-marks
+apt install -y golang-github-burntsushi-toml-dev
+
+apt install -y golang-github-influxdata-toml-dev
+
+apt install -y golang-github-knadh-koanf-dev
+
+apt install -y golang-github-komkom-toml-dev
+apt install -y golang-github-naoina-toml-dev
+
+apt install -y golang-github-nicksnyder-go-i18n.v2-dev
+
+apt install -y golang-github-pelletier-go-toml
+apt install -y golang-github-pelletier-go-toml-dev
+
+apt install -y golang-github-pelletier-go-toml.v2-dev
+
+apt install -y golang-github-spf13-cast-dev
+
+apt install -y golang-github-spf13-viper-dev
+
+apt install -y libcava-java
+
+apt install -y libcpptoml-dev
+
+apt install -y librust-system-deps-dev
+
+apt install -y librust-toml+indexmap-dev
+
+apt install -y librust-toml-dev
+
+apt install -y libtoml-parser-perl
+
+apt install -y libtoml-perl
+
+apt install -y libtoml-tiny-perl
+
+apt install -y libtoml11-dev
+
+apt install -y pyqt-builder-doc
+
+apt install -y python-cattrs-doc
+
+apt install -y python3-anymarkup
+
+apt install -y python3-cattr
+
+apt install -y python3-extension-helpers
+
+apt install -y python3-pyqtbuild
+
+apt install -y python3-pytoml
+
+apt install -y python3-toml
+
+apt install -y python3-tomli
+
+apt install -y python3-tomli-w
+
+apt install -y python3-tomlkit
+
+apt install -y python3-types-toml
+
+apt install -y python3-upstream-ontologist
+
+apt install -y r-cran-rcpptoml
+
+apt install -y ruby-asetus
+
+apt install -y ruby-toml
+
+apt install -y ruby-toml-rb
+
+apt install -y ruby-tomlrb
+
+apt install -y sanoid
