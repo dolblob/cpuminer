ce26be2ffa87e8b5fc825a1bec99a0b4f4a63f31
dolblob <estaparaelcs@gmail.com>
2026-02-12 22:05:29 +0100

    Create ostinato.sh

---
+++ ostinato.sh
@@ -0,0 +1,267 @@
+apt install -y ostinato
+
+apt install -y anyremote
+
+apt install -y anyremote-data
+
+apt install -y anyremote-doc
+
+apt install -y asterisk-mobile
+
+apt install -y ayatana-indicator-bluetooth
+
+apt install -y bettercap
+
+apt install -y bluedevil
+
+apt install -y blueman
+
+apt install -y bluemon
+
+apt install -y bluetooth
+apt install -y bluez
+
+apt install -y bluez-alsa-utils
+
+apt install -y bluez-btsco
+
+apt install -y bluez-cups
+apt install -y bluez-firmware
+
+apt install -y bluez-hcidump
+
+apt install -y bluez-meshd
+
+apt install -y bluez-obexd
+
+apt install -y bluez-tests
+
+apt install -y bluez-tools
+
+apt install -y btscanner
+
+apt install -y connman
+
+apt install -y connman-dev
+
+apt install -y connman-gtk
+apt install -y connman-vpn
+
+apt install -y dxtool
+
+apt install -y foxtrotgps
+
+apt install -y fuse-emulator-gtk
+
+apt install -y fuse-emulator-sdl
+
+apt install -y gammaray-plugin-bluetooth
+
+apt install -y ganyremote
+
+apt install -y gir1.2-gnomebluetooth-3.0
+
+apt install -y gir1.2-modemmanager-1.0
+
+apt install -y gnome-bluetooth
+
+apt install -y gnome-bluetooth-3-common
+
+apt install -y gnome-bluetooth-common
+
+apt install -y gnome-flashback
+
+apt install -y golang-github-jouyouyun-hardware-dev
+apt install -y golang-github-paypal-gatt-dev
+
+apt install -y gpodder
+
+apt install -y indicator-bluetooth
+
+apt install -y kanyremote
+
+apt install -y libasound2-plugin-bluez
+
+apt install -y libbluetooth-dev
+
+apt install -y libbluetooth3
+apt install -y libbtbb-dev
+
+apt install -y libbtbb1
+apt install -y libcwiid1
+apt install -y libgdbussyncevo0
+
+apt install -y libgnome-bluetooth-3.0-13
+
+apt install -y libgnome-bluetooth-3.0-dev
+
+apt install -y libgnome-bluetooth-dev
+
+apt install -y libgnome-bluetooth-doc
+
+apt install -y libgnome-bluetooth-ui-3.0-13
+
+apt install -y libgnome-bluetooth-ui-3.0-dev
+
+apt install -y libgnome-bluetooth13
+
+apt install -y libhidapi-dev
+
+apt install -y libhidapi-hidraw0
+
+apt install -y libhidapi-libusb0
+
+apt install -y libkf5bluezqt-data
+
+apt install -y libkf5bluezqt-doc
+
+apt install -y libkf5bluezqt6
+
+apt install -y libldacbt-abr-dev
+
+apt install -y libldacbt-abr2
+
+apt install -y libldacbt-enc-dev
+apt install -y libldacbt-enc2
+
+apt install -y libmm-glib-dev
+
+apt install -y libmm-glib-doc
+
+apt install -y libmm-glib0
+
+apt install -y libnet-bluetooth-perl
+
+apt install -y libo2
+
+apt install -y libo2-dev
+
+apt install -y libobexftp-perl
+
+apt install -y libobexftp0
+
+apt install -y libobexftp0-dev
+apt install -y libpam-blue
+
+apt install -y libqt5bluetooth5
+
+apt install -y libqt5bluetooth5-bin
+apt install -y libqt6bluetooth6
+
+apt install -y libqt6bluetooth6-bin
+
+apt install -y libreoffice
+
+apt install -y libspa-0.2-bluetooth
+
+apt install -y libsyncevo-dbus0
+
+apt install -y libsyncevolution0
+apt install -y libubertooth-dev
+
+apt install -y libubertooth1
+
+apt install -y modemmanager
+
+apt install -y modemmanager-dev
+apt install -y modemmanager-doc
+
+apt install -y nuntius
+
+apt install -y obexfs
+apt install -y obexftp
+
+apt install -y obexpushd
+
+apt install -y pd-comport
+
+apt install -y pi-bluetooth
+
+apt install -y pulseaudio-module-bluetooth
+apt install -y python3-bluez
+
+apt install -y python3-crccheck
+apt install -y python3-gattlib
+
+apt install -y python3-hid
+
+apt install -y python3-pyqt5.qtbluetooth
+
+apt install -y qml-module-org-kde-bluezqt
+
+apt install -y qml-module-qtbluetooth
+
+apt install -y qtconnectivity5-doc
+
+apt install -y qtconnectivity5-doc-html
+
+apt install -y rfkill
+apt install -y ruby-obexftp
+
+apt install -y sync-ui
+
+apt install -y syncevolution
+
+apt install -y syncevolution-common
+
+apt install -y syncevolution-dbus
+
+apt install -y syncevolution-http
+
+apt install -y syncevolution-libs
+
+apt install -y syncevolution-libs-gnome
+
+apt install -y tlp
+
+apt install -y tlp-rdw
+
+apt install -y totem-plugins
+
+apt install -y ubertooth
+
+apt install -y urfkill
+apt install -y ussp-push
+
+apt install -y ussp-push-dbg
+
+apt install -y wiipdf
+
+apt install -y dnscrypt-proxy
+
+apt install -y fscrypt
+apt install -y golang-github-elithrar-simple-scrypt-dev
+
+apt install -y golang-github-muesli-sasquatch-dev
+
+apt install -y golang-github-youmark-pkcs8-dev
+
+apt install -y hashcat
+
+apt install -y libghc-cryptonite-dev
+
+apt install -y libghc-cryptonite-doc
+
+apt install -y libghc-cryptonite-prof
+
+apt install -y libpam-fscrypt
+
+apt install -y libscrypt-dev
+
+apt install -y libscrypt-kdf-dev
+
+apt install -y libscrypt-kdf1
+
+apt install -y libscrypt0
+
+apt install -y node-randombytes
+
+apt install -y python-pycryptodome-doc
+
+apt install -y python3-pycryptodome
+
+apt install -y python3-scrypt
+
+apt install -y scrypt
+
+apt install -y silkaj
