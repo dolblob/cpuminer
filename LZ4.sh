0736d2d07b0308c509706c3868ce715afcd490c6
dolblob <estaparaelcs@gmail.com>
2026-03-14 10:42:51 +0100

    Update LZ4.sh

--- LZ4.sh
+++ LZ4.sh
@@ -54,3 +54,343 @@ apt install -y python3-roslz4
 
 apt install -y ruby-zoom
 apt install -y xarchiver
+
+
+apt install -y libjnr-posix-java
+
+apt install -y libjnr-posix-java-doc
+apt install -y libjoptsimple-java
+apt install -y libjoptsimple-java-doc
+
+apt install -y libjruby-joni-java
+
+apt install -y liblinux-acl-perl
+
+apt install -y liblinux-termios2-perl
+
+apt install -y libmfsio-dev
+
+apt install -y libmfsio1
+
+apt install -y libmono-posix4.0-cil
+
+apt install -y libmorbig-ocaml-dev
+
+apt install -y libmorsmall-ocaml-dev
+
+apt install -y libmtp-common
+
+apt install -y libmtp-dev
+
+apt install -y libmtp-doc
+
+apt install -y libmtp-runtime
+
+apt install -y libmtp9
+
+apt install -y libnode72
+
+apt install -y liboop-dev
+
+apt install -y liboop-doc
+
+apt install -y liboop4
+
+apt install -y libopenthreads-dev
+apt install -y libqsastime0
+
+apt install -y libre-dev
+
+apt install -y libre-ocaml-dev
+
+apt install -y libre0
+apt install -y librem-dev
+
+apt install -y librem0
+
+apt install -y librust-ioctl-sys-dev
+apt install -y librust-onig+posix-api-dev
+
+apt install -y libsendmail-pmilter-perl
+
+apt install -y libserial-dev
+
+apt install -y libserial-doc
+
+apt install -y libserial1
+
+apt install -y libshell-posix-select-perl
+
+apt install -y libst-dev
+
+apt install -y libst1
+
+apt install -y libstring-crc-cksum-perl
+
+apt install -y libsys-mmap-perl
+
+apt install -y libtar0
+apt install -y libtemplate-plugin-posix-perl
+
+apt install -y libterm-readline-zoid-perl
+
+apt install -y libthread-sigmask-perl
+apt install -y libtoml11-dev
+
+apt install -y libtre-dev
+
+apt install -y libtre5
+
+apt install -y libucommon-dev
+
+apt install -y libucommon8
+
+apt install -y libui-utilcpp-dev
+
+apt install -y libui-utilcpp9v5
+
+apt install -y libwebsocketpp-dev
+
+apt install -y libwebsocketpp-doc
+
+apt install -y libxrdcephposix0
+
+apt install -y libxrdposix3
+
+apt install -y libzc-dev
+
+apt install -y libzc6
+
+apt install -y libzthread-2.3-2
+
+apt install -y libzthread-dev
+apt install -y lua-curses
+
+apt install -y lua-iconv
+
+apt install -y lua-posix
+
+apt install -y lua-posix-dev
+
+apt install -y lua-rex-posix
+
+apt install -y lua-rex-posix-dev
+
+apt install -y manpages-posix
+
+apt install -y manpages-posix-dev
+
+apt install -y mawk
+
+apt install -y mk-configure
+
+apt install -y mksh
+
+apt install -y moosefs-cgi
+
+apt install -y moosefs-cgiserv
+
+apt install -y moosefs-chunkserver
+
+apt install -y moosefs-cli
+
+apt install -y moosefs-client
+
+apt install -y moosefs-common
+
+apt install -y moosefs-master
+
+apt install -y moosefs-metalogger
+
+apt install -y moosefs-netdump
+
+apt install -y morbig
+
+apt install -y mtp-tools
+
+apt install -y nco
+
+apt install -y nfs-ganesha
+
+apt install -y nfs-ganesha-ceph
+apt install -y nfs-ganesha-doc
+
+apt install -y nfs-ganesha-gluster
+
+apt install -y nfs-ganesha-gpfs
+apt install -y nfs-ganesha-mem
+
+apt install -y nfs-ganesha-mount-9p
+
+apt install -y nfs-ganesha-nullfs
+
+apt install -y nfs-ganesha-proxy
+
+apt install -y nfs-ganesha-rados-grace
+
+apt install -y nfs-ganesha-rgw
+
+apt install -y nfs-ganesha-vfs
+
+apt install -y node-expand-brackets
+
+apt install -y node-extsprintf
+
+apt install -y node-path-dirname
+
+apt install -y node-path-root
+
+apt install -y node-path-root-regex
+
+apt install -y node-posix-character-classes
+
+apt install -y node-posix-getopt
+
+apt install -y node-tar
+
+apt install -y node-wcwidth.js
+
+apt install -y nodejs
+
+apt install -y nodejs-doc
+
+apt install -y nq
+
+apt install -y paprefs
+
+apt install -y pavumeter
+
+apt install -y pcregrep
+
+apt install -y perdition
+
+apt install -y posixtestsuite
+
+apt install -y printemf
+apt install -y pulseaudio
+
+apt install -y pulseaudio-equalizer
+
+apt install -y pulseaudio-module-bluetooth
+
+apt install -y pulseaudio-module-gsettings
+
+apt install -y pulseaudio-module-jack
+
+apt install -y pulseaudio-module-lirc
+
+apt install -y pulseaudio-module-raop
+apt install -y pulseaudio-module-zeroconf
+
+apt install -y pulseaudio-utils
+
+apt install -y puppet-module-voxpupuli-posix-acl
+
+apt install -y python-h5py-doc
+
+apt install -y python-pathvalidate-doc
+
+apt install -y python-posix-ipc
+
+apt install -y python-pylibacl-doc
+
+apt install -y python-rpaths-doc
+
+apt install -y python3-atomicwrites
+
+apt install -y python3-azure-datalake-store
+
+apt install -y python3-cap-ng
+
+apt install -y python3-crccheck
+
+apt install -y python3-cwcwidth
+
+apt install -y python3-h5py
+apt install -y python3-h5py-mpi
+
+apt install -y python3-h5py-serial
+
+apt install -y python3-lockfile
+
+apt install -y python3-nfs-ganesha
+
+apt install -y python3-parallel
+
+apt install -y python3-pathvalidate
+
+apt install -y python3-posix-ipc
+
+apt install -y python3-pylibacl
+
+apt install -y python3-pytest-timeout
+
+apt install -y python3-rpaths
+
+apt install -y python3-scantree
+
+apt install -y python3-serial
+
+apt install -y python3-terminaltables
+
+apt install -y r-cran-ff
+
+apt install -y r-cran-nanotime
+
+apt install -y r-cran-pcict
+
+apt install -y r-cran-units
+
+apt install -y raku-getopt-long
+
+apt install -y restartd
+
+apt install -y ruby-hitimes
+
+apt install -y ruby-minitar
+
+apt install -y ruby-posix-spawn
+
+apt install -y ruby-raindrops
+
+apt install -y ruby-spoon
+
+apt install -y ruby-terrapin
+
+apt install -y runoverssh
+
+apt install -y s-nail
+
+apt install -y scdoc
+
+apt install -y scm
+apt install -y scotch
+
+apt install -y scrub
+
+apt install -y shfmt
+
+apt install -y smbldap-tools
+
+apt install -y standardskriver
+
+apt install -y stress
+
+apt install -y susv3
+
+apt install -y susv4
+apt install -y sysbench
+
+apt install -y tdc
+
+apt install -y ucommon-doc
+
+apt install -y ucommon-utils
+
+apt install -y xrootd-server
+
+apt install -y yash
+
+apt install -y yorick-yeti-regex
+
+apt install -y zfs-auto-snapshot
